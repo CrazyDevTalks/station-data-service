8f91d622222e63412ab230c76340c3e8e0bae6c9
CrazyDev <crazydev0206@gmail.com>
2017-12-30 21:42:15 -0500

    Initial

---
+++ src/main/java/com/merchantnottingham/littlejohnbacktestservice/BacktestController.java
@@ -0,0 +1,4 @@
+package com.merchantnottingham.littlejohnbacktestservice;
+
+public class BacktestController {
+}
---
+++ src/main/java/com/merchantnottingham/littlejohnbacktestservice/LittlejohnBacktestServiceApplication.java
@@ -0,0 +1,12 @@
+package com.merchantnottingham.littlejohnbacktestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittlejohnBacktestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittlejohnBacktestServiceApplication.class, args);
+	}
+}
