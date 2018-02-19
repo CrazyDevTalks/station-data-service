cfe5e79d4d5e14d2bd4636ef0fa190047d628400
CrazyDev <crazydev0206@gmail.com>
2018-02-19 02:30:32 -0500

    Added ability to update db

--- src/main/java/com/merchantnottingham/littlejohnbacktestservice/quotes/QuoteServiceImpl.java
+++ src/main/java/com/merchantnottingham/littlejohnbacktestservice/quotes/QuoteServiceImpl.java
@@ -5,7 +5,6 @@ import com.merchantnottingham.littlejohnbacktestservice.LittlejohnBacktestServic
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.config.ResourceNotFoundException;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
@@ -17,7 +16,7 @@ import java.util.concurrent.TimeUnit;
 
 @Component("quoteService")
 public class QuoteServiceImpl
-    implements QuoteService
+        implements QuoteService
 {
     @Autowired
     private QuoteRepository quoteRepo;
@@ -30,8 +29,8 @@ public class QuoteServiceImpl
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.setContentType(MediaType.APPLICATION_JSON);
 
-        Date start = toMidnight(from, -1);
-        Date end = toMidnight(to, 25);
+        Date start = toTradeDay(from, -1);
+        Date end = toTradeDay(to, 25);
 
         symbol = symbol.toUpperCase();
         List<Quote> quotes = quoteRepo.findBySymbolAndDateBetween(symbol, start, end);
@@ -41,45 +40,62 @@ public class QuoteServiceImpl
         long days = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
         log.info("DB results: {}, expected results: {} ", (double) quotes.size(), days);
 
-        if ((double) quotes.size() - ((days * 0.685) + 1.0) >= 0.0
-//                &&
-//                toMidnight(queried.get(0).getDate(), 0) == toMidnight(from, 0) &&
-//                toMidnight(queried.get(queried.size() - 1).getDate(), 0) == toMidnight(to, 0)
-                ) {
+        if ((double) quotes.size() - ((days * 0.685) + 1.0) >= 0.0) {
             log.info("Using DB results");
+
+            log.info("start date: {}, end date: {}", quotes.get(0).getDate(), quotes.get(quotes.size() - 1).getDate());
+            return new ResponseEntity<>(quotes,
+                    responseHeaders, HttpStatus.OK);
+        } else if (quotes.size() > 0 && start.compareTo(toTradeDay(quotes.get(0).getDate(), 0)) >= 0 &&
+                end.compareTo(toTradeDay(quotes.get(quotes.size() - 1).getDate(), 0)) > 0
+                ) {
+            log.info("Updating DB results");
+
+            log.info("new start date: {}, end date: {}", toTradeDay(quotes.get(quotes.size() - 1).getDate(), 0), end);
+            addQuotes(symbol, toTradeDay(quotes.get(quotes.size() - 1).getDate(), 0), end);
 
             return new ResponseEntity<>(quotes,
                     responseHeaders, HttpStatus.OK);
-        } else {
+        }
+        else {
             quoteRepo.delete(quotes);
-            RestTemplate restTemplate = new RestTemplate();
-
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-
-            SimpleDateFormat dt1 = new SimpleDateFormat("yyyy-MM-dd");
-
-            HttpEntity<String> entity = new HttpEntity<>("{\"ticker\": \""+symbol+"\", \"start\": \"" + dt1.format(from) + "\", \"end\": \"" + dt1.format(to) + "\"}", headers);
-
-            Quote[] response = restTemplate.postForObject("http://localhost:9000/api/quote", entity, Quote[].class);
-
-            quotes = Arrays.asList(response);
-
-            quoteRepo.save(quotes);
-            log.info("Saved results");
-
+            addQuotes(symbol, from, to);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
-    private Date toMidnight(Date date, int modifier) {
+    private void addQuotes(String symbol, Date from, Date to) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        SimpleDateFormat dt1 = new SimpleDateFormat("yyyy-MM-dd");
+
+        HttpEntity<String> entity = new HttpEntity<>("{\"ticker\": \""+symbol+"\", \"start\": \"" + dt1.format(from) + "\", \"end\": \"" + dt1.format(to) + "\"}", headers);
+
+        Quote[] response = restTemplate.postForObject("http://localhost:9000/api/quote", entity, Quote[].class);
+
+        List<Quote> quotes = Arrays.asList(response);
+
+        quoteRepo.save(quotes);
+        log.info("Saved {} results", quotes.size());
+    }
+
+    private Date toTradeDay(Date date, int hourModifier) {
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
         cal.set(Calendar.HOUR_OF_DAY, 0);
         cal.set(Calendar.MINUTE, 0);
         cal.set(Calendar.SECOND, 0);
         cal.set(Calendar.MILLISECOND,0);
-        cal.add(Calendar.HOUR_OF_DAY, modifier);
+        cal.add(Calendar.HOUR_OF_DAY, hourModifier);
+
+        if ((cal.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY)) {
+            cal.add(Calendar.DATE, -1);
+        } else if ((cal.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY)){
+            cal.add(Calendar.DATE, -2);
+        }
 
         return cal.getTime();
     }
